6ef5c64c241c8f3bf033eb31674bb65fe2e86c63
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-07-05 21:48:11 +0200

    Reorganize component

--- apps/website/src/routes/_index.tsx
+++ apps/website/src/routes/_index.tsx
@@ -25,14 +25,16 @@ const IndexAppRoute = () => {
 	const { date } = useLoaderData<typeof loader>();
 
 	return (
-		<div>
-			<h1 style={css({ color: "blue", on: $ => [$("hover", { color: "red" })] })}>Hello world</h1>
+		<>
 			<title>bla bla</title>
 			<meta content="Welcome to Remix!" name="description" />
-			<Suspense fallback={<p>Loading...</p>}>
-				<Test datePromise={date} />
-			</Suspense>
-		</div>
+			<div>
+				<h1 style={css({ color: "blue", on: $ => [$("hover", { color: "red" })] })}>Hello world</h1>
+				<Suspense fallback={<p>Loading...</p>}>
+					<Test datePromise={date} />
+				</Suspense>
+			</div>
+		</>
 	);
 };
 
