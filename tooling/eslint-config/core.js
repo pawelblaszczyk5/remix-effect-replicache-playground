8d3c82013804c8b9bbf116c7f5504d972d31588c
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-07-10 19:55:36 +0200

    Enable new rule

--- tooling/eslint-config/core.js
+++ tooling/eslint-config/core.js
@@ -200,6 +200,7 @@ export default tseslint.config(
 					],
 				},
 			],
+			"@typescript-eslint/no-unnecessary-type-parameters": "error",
 			"@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
 			"@typescript-eslint/no-use-before-define": [
 				"error",
