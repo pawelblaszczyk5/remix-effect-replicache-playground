2386151ea9bb0092dd0d816992386ede6309b796
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-08-10 20:12:24 +0200

    Remove example route with param

--- apps/website/src/routes/app.$todo/route.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "@remix-run/react";
-
-const Route = () => {
-	const params = useParams();
-
-	return <h1>Hello todo app {params["todo"]}</h1>;
-};
-
-export default Route;
