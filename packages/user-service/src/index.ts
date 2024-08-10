20876f2b73346fdd413e28a0d30a6738b4cec922
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-08-10 16:42:34 +0200

    Implement stuff around simple login/logout

--- packages/user-service/src/index.ts
+++ packages/user-service/src/index.ts
@@ -1,9 +1,36 @@
-import { Context, Effect, Layer } from "effect";
+import { createFileSessionStorage } from "@remix-run/node";
+import { Context, Duration, Effect, Layer } from "effect";
+
+import { RemixRequest } from "@repo/request-context";
 
 const makeUserServiceLive = () => {
+	const { commitSession, destroySession, getSession } = createFileSessionStorage<
+		{ name: string },
+		Record<never, string>
+	>({
+		cookie: {
+			httpOnly: true,
+			maxAge: Duration.days(10).pipe(Duration.toSeconds),
+			name: "__session__",
+			path: "/",
+			sameSite: "lax",
+			secure: true,
+		},
+		dir: "./sessions",
+	});
+
 	return {
-		greet: (name?: string) => {
+		greet: () => {
 			return Effect.gen(function* () {
+				const request = yield* RemixRequest;
+				const cookieHeader = request.headers.get("Cookie");
+
+				const session = yield* Effect.tryPromise(async () => {
+					return getSession(cookieHeader);
+				});
+
+				const name = session.get("name");
+
 				if (name) {
 					return `Hello ${name}!`;
 				}
@@ -11,6 +38,36 @@ const makeUserServiceLive = () => {
 				yield* Effect.log("Anonymous user visited!");
 
 				return "Hello stranger!";
+			});
+		},
+		login: (name: string) => {
+			return Effect.gen(function* () {
+				const request = yield* RemixRequest;
+				const cookieHeader = request.headers.get("Cookie");
+
+				const cookie = yield* Effect.tryPromise(async () => {
+					const session = await getSession(cookieHeader);
+
+					session.set("name", name);
+
+					return commitSession(session);
+				});
+
+				return cookie;
+			});
+		},
+		logout: () => {
+			return Effect.gen(function* () {
+				const request = yield* RemixRequest;
+				const cookieHeader = request.headers.get("Cookie");
+
+				const cookie = yield* Effect.tryPromise(async () => {
+					const session = await getSession(cookieHeader);
+
+					return destroySession(session);
+				});
+
+				return cookie;
 			});
 		},
 	};
