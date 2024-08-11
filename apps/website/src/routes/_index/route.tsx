c464769ba2eba599643a73abba258d10bf324912
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-08-11 19:22:06 +0200

    App user implementation

--- apps/website/src/routes/_index/route.tsx
+++ apps/website/src/routes/_index/route.tsx
@@ -7,30 +7,43 @@ import { UserService } from "@repo/user-service";
 
 export const loader = defineEffectLoader(
 	Effect.gen(function* () {
-		const exampleService = yield* UserService;
+		const userService = yield* UserService;
+		const user = yield* userService.getUser();
+		const greeting = yield* userService.greet();
 
-		return yield* exampleService.greet();
+		return { greeting, user };
 	}),
 );
 
 const Route = () => {
-	const greeting = useLoaderData<typeof loader>();
+	const { greeting, user } = useLoaderData<typeof loader>();
 
 	return (
 		<>
 			<title>Example home page</title>
 			<meta content="Welcome to example!" name="description" />
+			<h1
+				style={css({
+					color: "blue",
+					on: ($) => {
+						return [$("hover", { color: "red" })];
+					},
+				})}
+			>
+				{greeting}
+			</h1>
 			<div>
-				<h1
-					style={css({
-						color: "blue",
-						on: ($) => {
-							return [$("hover", { color: "red" })];
-						},
-					})}
-				>
-					{greeting}
-				</h1>
+				{user ? (
+					<Link prefetch="render" to={{ pathname: "/logout" }}>
+						Logout form
+					</Link>
+				) : (
+					<Link prefetch="render" to={{ pathname: "/login" }}>
+						Login form
+					</Link>
+				)}
+			</div>
+			<div>
 				<Link prefetch="render" to={{ pathname: "/app" }}>
 					Go to app
 				</Link>
--- apps/website/src/routes/app/route.tsx
+++ apps/website/src/routes/app/route.tsx
@@ -1,14 +1,46 @@
-import { Outlet } from "@remix-run/react";
+import type { ClientLoaderFunctionArgs } from "@remix-run/react";
+
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { Effect } from "effect";
+
+import { Redirect } from "@repo/effect-errors";
+import { defineEffectLoader } from "@repo/effect-runtime";
+import { UserService } from "@repo/user-service";
 
 import { ReplicacheProvider } from "#src/lib/replicache.client.js";
 
-export const clientLoader = () => {
-	return null;
+export const loader = defineEffectLoader(
+	Effect.gen(function* () {
+		const userService = yield* UserService;
+
+		const user = yield* userService.getUser();
+
+		if (!user) {
+			return yield* new Redirect({ url: "/login" });
+		}
+
+		return user;
+	}),
+);
+
+export const clientLoader = async ({ serverLoader }: ClientLoaderFunctionArgs) => {
+	const data = await serverLoader<typeof loader>();
+
+	if (typeof data === "object") {
+		// eslint-disable-next-line @typescript-eslint/only-throw-error -- it's fine there
+		throw data;
+	}
+
+	return { user: data };
 };
 
+clientLoader.hydrate = true;
+
 const Route = () => {
+	const { user } = useLoaderData<typeof clientLoader>();
+
 	return (
-		<ReplicacheProvider userId="John Doe">
+		<ReplicacheProvider userId={user}>
 			<title>App</title>
 			<Outlet />
 		</ReplicacheProvider>
