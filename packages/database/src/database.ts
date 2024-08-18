c9c4fe993289a7f1c3f59b863806d04c24b84566
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-08-18 20:10:46 +0200

    WIP mechanism for running migrations

--- packages/database/src/database.ts
+++ packages/database/src/database.ts
@@ -4,7 +4,7 @@ import { createClient } from "@libsql/client";
 import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
-import { Config, Context, Data, Effect, Layer } from "effect";
+import { Config, Context, Data, Effect, Exit, Layer } from "effect";
 
 import { cvr, replicacheClient, replicacheClientGroup, todo } from "#src/schema.js";
 
@@ -56,6 +56,61 @@ export class Database extends Context.Tag("@repo/database#Database")<Database, R
 			return database;
 		}),
 	);
+}
+
+type TransactionDatabaseInstance = Parameters<Parameters<ReturnType<typeof makeDatabaseLive>["transaction"]>[0]>[0];
+
+export class TransactionDatabase extends Context.Tag("@repo/database#TransactionDatabase")<
+	TransactionDatabase,
+	TransactionDatabaseInstance
+>() {
+	static readonly provideTransaction = <A, E, R>(self: Effect.Effect<A, E, R>) => {
+		return Effect.gen(function* () {
+			const database = yield* Database;
+			const transaction = Promise.withResolvers();
+
+			return yield* Effect.acquireUseRelease(
+				Effect.gen(function* () {
+					const databaseAcquire = Promise.withResolvers<TransactionDatabaseInstance>();
+
+					database
+						.transaction(async (tx) => {
+							databaseAcquire.resolve(tx);
+
+							return transaction.promise;
+						})
+						.catch(() => {
+							databaseAcquire.reject();
+						});
+
+					return yield* Effect.tryPromise(async () => {
+						return databaseAcquire.promise;
+					});
+				}),
+				(database) => {
+					return self.pipe(
+						// NOTE providing this instance as standard Database as well so it's overwritten in nested context accesses, it's not fully type-safe, but leaving it as is for now
+						Effect.provideService(Database, database as unknown as Context.Tag.Service<Database>),
+						Effect.provideService(TransactionDatabase, database),
+					);
+				},
+				(_, exit) => {
+					Exit.match(exit, {
+						onFailure: (value) => {
+							transaction.reject(value);
+						},
+						onSuccess: (value) => {
+							transaction.resolve(value);
+						},
+					});
+
+					return Effect.tryPromise(async () => {
+						return transaction.promise;
+					}).pipe(Effect.orDie);
+				},
+			);
+		});
+	};
 }
 
 export * from "drizzle-orm/sql";
