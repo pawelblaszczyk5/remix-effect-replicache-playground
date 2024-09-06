15c8fd79885286d7993ddc7984af661e95029b67
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-09-06 20:30:37 +0200

    Experimental Hono setup

--- apps/website/src/entry.server.tsx
+++ apps/website/src/entry.server.tsx
@@ -6,6 +6,7 @@ import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { PassThrough } from "node:stream";
 import { renderToPipeableStream } from "react-dom/server";
+import { createHonoServer } from "react-router-hono-server/node";
 
 const ABORT_DELAY = 5_000;
 
@@ -65,3 +66,5 @@ const handleRequest = async (
 };
 
 export default handleRequest;
+
+export const server = await createHonoServer();
--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,29 +1,34 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { devServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 
-export default defineConfig({
-	plugins: [
-		remix({
-			appDirectory: "src",
-			future: {
-				unstable_lazyRouteDiscovery: true,
-				unstable_singleFetch: true,
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-			serverModuleFormat: "esm",
-		}),
-		babel({
-			babelConfig: {
-				plugins: [["babel-plugin-react-compiler", {}]],
-				presets: ["@babel/preset-typescript"],
-			},
-			filter: /\.[jt]sx?$/u,
-		}),
-	],
-	ssr: {
-		external: ["better-sqlite3"],
-	},
+export default defineConfig(({ isSsrBuild }) => {
+	return {
+		...(isSsrBuild && { build: { target: "esnext" } }),
+		plugins: [
+			devServer({ appDirectory: "src", entry: "src/entry.server.tsx" }),
+			remix({
+				appDirectory: "src",
+				future: {
+					unstable_lazyRouteDiscovery: true,
+					unstable_singleFetch: true,
+					v3_fetcherPersist: true,
+					v3_relativeSplatPath: true,
+					v3_throwAbortReason: true,
+				},
+				serverModuleFormat: "esm",
+			}),
+			babel({
+				babelConfig: {
+					plugins: [["babel-plugin-react-compiler", {}]],
+					presets: ["@babel/preset-typescript"],
+				},
+				filter: /\.[jt]sx?$/u,
+			}),
+		],
+		ssr: {
+			external: ["better-sqlite3"],
+		},
+	};
 });
