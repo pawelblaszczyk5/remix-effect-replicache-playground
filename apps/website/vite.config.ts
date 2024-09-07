a51606de4bbdd8c5d9822ce3e8ec809a3172aaa4
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-09-07 20:20:17 +0200

    Simplify build target

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -3,32 +3,30 @@ import { devServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 
-export default defineConfig(({ isSsrBuild }) => {
-	return {
-		...(isSsrBuild && { build: { target: "esnext" } }),
-		plugins: [
-			devServer({ appDirectory: "src", entry: "src/entry.server.tsx" }),
-			remix({
-				appDirectory: "src",
-				future: {
-					unstable_lazyRouteDiscovery: true,
-					unstable_singleFetch: true,
-					v3_fetcherPersist: true,
-					v3_relativeSplatPath: true,
-					v3_throwAbortReason: true,
-				},
-				serverModuleFormat: "esm",
-			}),
-			babel({
-				babelConfig: {
-					plugins: [["babel-plugin-react-compiler", {}]],
-					presets: ["@babel/preset-typescript"],
-				},
-				filter: /\.[jt]sx?$/u,
-			}),
-		],
-		ssr: {
-			external: ["better-sqlite3"],
-		},
-	};
+export default defineConfig({
+	build: { target: "esnext" },
+	plugins: [
+		devServer({ appDirectory: "src", entry: "src/entry.server.tsx" }),
+		remix({
+			appDirectory: "src",
+			future: {
+				unstable_lazyRouteDiscovery: true,
+				unstable_singleFetch: true,
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+			},
+			serverModuleFormat: "esm",
+		}),
+		babel({
+			babelConfig: {
+				plugins: [["babel-plugin-react-compiler", {}]],
+				presets: ["@babel/preset-typescript"],
+			},
+			filter: /\.[jt]sx?$/u,
+		}),
+	],
+	ssr: {
+		external: ["better-sqlite3"],
+	},
 });
